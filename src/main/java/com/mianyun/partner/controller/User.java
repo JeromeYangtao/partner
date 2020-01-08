28f2be32fadaf738f3b7105cd16a94ff2c73efb4
Thomson <yangtao135@foxmail.com>
2020-01-08 20:58:24 +0800

    feat: user 增删改查

--- src/main/java/com/mianyun/partner/controller/User.java
+++ src/main/java/com/mianyun/partner/controller/User.java
@@ -1,18 +1,45 @@
 package com.mianyun.partner.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("")
 public class User {
 
-    @RequestMapping("")
+    @GetMapping(value = "/users")
     public ArrayList getUserList(){
         ArrayList<String> userList = new ArrayList<>();
         userList.add("测试用户");
+        return userList;
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ArrayList getUser(@PathVariable Long id){
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("测试用户" + id);
+        return userList;
+    }
+
+    @PostMapping("/user")
+    public ArrayList addUser(){
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("测试用户添加");
+        return userList;
+    }
+
+    @PutMapping("/user/{id}")
+    public ArrayList updateUser(@PathVariable Long id){
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("测试用户更新" + id);
+        return userList;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ArrayList deleteUser(@PathVariable Long id){
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("测试用户删除" + id);
         return userList;
     }
 }
