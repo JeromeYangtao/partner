42f8ffd09f7ef6b42107574455bf65acbe2c0f2f
JeromeYangtao <779387850@qq.com>
2020-01-27 23:25:49 +0800

    feat: class 热更新

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 SpringBoot + Mybatis 练手项目
+
+
+已完成功能
+- [x] 模板引擎整合前端
+- [x] RestFul接口
+- [x] class热更新
--- src/main/java/com/mianyun/partner/Controller/UserController.java
+++ src/main/java/com/mianyun/partner/Controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class UserController {
 
     @GetMapping(value = "/users")
